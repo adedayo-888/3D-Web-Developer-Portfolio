f190d062fd15db058beac422ae341dbe1c194f84
adedayo888 <dayoadeniji69@gmail.com>
2023-04-05 18:37:17 +0100

    set up frontend with rea ct and tailwindccs

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1 className='text-6xl underline'>web</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
